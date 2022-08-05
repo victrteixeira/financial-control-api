db3214a2d54e062c4da3ead775d92f7727b61cdf
Victor <vicktorteixeira.7@gmail.com>
2022-08-04 22:01:59 -0300

    Interfaces to class services

---
+++ Challenge.Services/Interfaces/IDespesaService.cs
@@ -0,0 +1,15 @@
+﻿using Challenge.Services.DTO;
+
+namespace Challenge.Services.Interfaces;
+
+public interface IDespesaService
+{
+    Task<DespesasDTO> CreateAsync(DespesasDTO value);
+    Task<DespesasDTO> UpdateAsync(DespesasDTO value);
+    Task RemoveAsync(long id);
+    Task<DespesasDTO> GetAsync(long id);
+    Task<List<DespesasDTO>> GetAllAsync();
+    Task<List<DespesasDTO>> GetAllByValorAsync(double value);
+    Task<List<DespesasDTO>> SearchByDescricaoAsync(string value);
+    Task<List<DespesasDTO>> SearchByMonthAsync(string value);
+}
---
+++ Challenge.Services/Interfaces/IReceitaService.cs
@@ -0,0 +1,15 @@
+﻿using Challenge.Services.DTO;
+
+namespace Challenge.Services.Interfaces;
+
+public interface IReceitaService
+{
+    Task<ReceitasDTO> CreateAsync(ReceitasDTO value);
+    Task<ReceitasDTO> UpdateAsync(ReceitasDTO value);
+    Task RemoveAsync(long id);
+    Task<ReceitasDTO> GetAsync(long id);
+    Task<List<ReceitasDTO>> GetAllAsync();
+    Task<List<ReceitasDTO>> GetAllByValorAsync(double value);
+    Task<List<ReceitasDTO>> SearchByDescricaoAsync(string value);
+    Task<List<ReceitasDTO>> SearchByMonthAsync(string value);
+}
